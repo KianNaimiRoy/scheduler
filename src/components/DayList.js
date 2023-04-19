c9c393223b456b4edeee8fb72e43813256b3c527
Kian <kiannaimiroy@gmail.com>
2023-04-18 20:15:28 -0700

    created Daylist component as a container for DayListItem

---
+++ src/components/DayList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+  const mappedDays = props.days.map((day) => {
+    return <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      setDay={props.setDay}
+      selected={props.day === day.name}
+    />;
+
+  });
+
+  return <ul>
+    {mappedDays}
+  </ul>;
+}
+
+
