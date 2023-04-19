708844bd85fb690e64a60324850d6eea235fddb4
Kian <kiannaimiroy@gmail.com>
2023-04-19 16:38:49 -0700

    create Appointment.js as well as .scss and added stories for it.

--- stories/index.js
+++ stories/index.js
@@ -5,6 +5,7 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index.js";
 import "index.scss";
 
 import Button from "components/Button";
@@ -130,3 +131,10 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+  storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm"/>)
